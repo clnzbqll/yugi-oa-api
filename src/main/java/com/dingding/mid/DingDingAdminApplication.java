ba5a23a525f306936df1111de00844b31afb4c7a
clnzbqll <1500841683@qq.com>
2024-08-07 17:36:47 +0800

    # 删除工具类，枚举，常数

--- src/main/java/com/dingding/mid/DingDingAdminApplication.java
+++ src/main/java/com/dingding/mid/DingDingAdminApplication.java
@@ -1,24 +1,8 @@
 package com.dingding.mid;
 
-import java.io.*;
-import java.lang.reflect.Method;
-import java.util.concurrent.ConcurrentHashMap;
-
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.io.IoUtil;
-import com.dingding.mid.utils.MinioUploadUtil;
-import com.dingding.mid.utils.SpringContextHolder;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import lombok.Cleanup;
-import org.flowable.common.engine.impl.de.odysseus.el.ExpressionFactoryImpl;
-import org.flowable.common.engine.impl.de.odysseus.el.util.SimpleContext;
-import org.flowable.common.engine.impl.javax.el.ExpressionFactory;
-import org.flowable.common.engine.impl.javax.el.ValueExpression;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
