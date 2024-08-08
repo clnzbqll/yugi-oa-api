b4436d0bd866ee28abdfaeebf827af3b155e89fd
clnzbqll <1500841683@qq.com>
2024-08-08 14:39:53 +0800

    # 精简代码

--- src/main/java/com/dingding/mid/DingDingAdminApplication.java
+++ src/main/java/com/dingding/mid/DingDingAdminApplication.java
@@ -1,29 +1,12 @@
 package com.dingding.mid;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
-/**
- */
-@SpringBootApplication(scanBasePackages = "com")
-public class DingDingAdminApplication extends SpringBootServletInitializer implements
-    CommandLineRunner {
-
-    public static void main(String[] args)  {
-        SpringApplication springApplication = new SpringApplication(DingDingAdminApplication.class);
-        springApplication.run(args);
-        System.out.println("DingDing启动完成");
+@SpringBootApplication
+public class DingDingAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DingDingAdminApplication.class, args);
+        System.out.println("启动成功");
     }
-
-
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
-
-
-
 }
